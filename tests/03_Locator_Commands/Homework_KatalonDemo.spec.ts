998d3805a4256635a3f1736c93f0df05bd9da9a6
vanshdeepkewlani <vkewlani@gmail.com>
2026-09-16 18:15:57 +0530

    Add Katalon demo locator homework test

---
+++ tests/03_Locator_Commands/Homework_KatalonDemo.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://katalon-demo-cura.herokuapp.com/');
+  await page.getByRole('link', { name: 'Make Appointment' }).click();
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('John Doe');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('ThisIsNotAPassword');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.locator('h2')).toContainText('Make Appointment');
+  await page.close();
+});
